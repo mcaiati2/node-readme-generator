fa05f805fc2f6c500a8ec014f7e17962d1246f7c
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-10-03 21:27:58 -0400

    folders organized

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,16 @@
-// // TODO: Include packages needed for this application
+// TODO: Include packages needed for this application
 
-// // TODO: Create an array of questions for user input
-// const questions = [];
+// TODO: Create an array of questions for user input
+const questions = [];
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
-// // TODO: Create a function to initialize app
-// function init() {}
+// TODO: Create a function to initialize app
+function init() {}
 
-// // Function call to initialize app
-// init();
+// Function call to initialize app
+init();
 
 
 import inquirer from 'inquirer';
--- contact_info.md
+++
@@ -1,7 +0,0 @@
-
-# Contact Information
-
-- **Full Name:** Michael Caiati
-- **Phone Number:** 555-555-5555
-- **Address:** 123 test street
-- **Favorite Food Category:** Chinese
